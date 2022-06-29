8737dab27c11898fe50c598a531523d12927f2b2
Kate <kate@MacBook-Air-Kate.local>
2022-06-29 16:54:04 +0200

    Feat: add decision of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let i = 0;
+    let result = '';
+    let str = String(n);
+    if (n < 0) {
+        str = String(n * -1);
+    } else {
+        str = String(n);
+    }
+    while (i < str.length) {
+        result = `${str[i]}${result}`;
+        i = i + 1;
+    }
+        return result;
+    }
